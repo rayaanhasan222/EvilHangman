5d59f7ad34cc4c071545d750277ed56e24254799
drospierski24 <92823813+drospierski24@users.noreply.github.com>
2021-10-26 19:39:41 -0500

    Update HangmanManager.java

--- HangmanManager.java
+++ HangmanManager.java
@@ -50,6 +50,67 @@ public class HangmanManager
 	
 	public int record( char guess )
 	{
-		return 0;
+		 if (wordsConsidered.isEmpty() || guessCount < 1) {
+            throw new IllegalStateException();
+        } else if (!wordsConsidered.isEmpty() && lettersGuessed.contains(guess)) {
+            throw new IllegalArgumentException();
+        }
+        lettersGuessed.add(guess);
+        Map<String, Set<String>> findFamily = new TreeMap<String, Set<String>>();
+        for (String word : wordsConsidered) {
+            String pattern = patternMaker(guess, word);
+            Set<String> setOfWords = new TreeSet<String>();
+            if (!findFamily.containsKey(pattern)) {
+                findFamily.put(pattern, setOfWords);
+            }
+            findFamily.get(pattern).add(word);
+        }
+        int maximumSize = 0;
+        if (!findFamily.isEmpty()) {
+            for (String currentPattern : findFamily.keySet()) {
+                if (findFamily.get(currentPattern).size() > maximumSize) {
+                    wordsConsidered.clear();
+                    wordsConsidered.addAll(findFamily.get(currentPattern));
+                    patternDisplayed = currentPattern;
+                    maximumSize = findFamily.get(currentPattern).size();
+                }
+            }
+        }
+        return decision(guess);
+    }
+
+    // This method keeps updating the pattern with each input
+    // dashes represent unidentified letter
+    // returns the pattern going to be displayed
+    private String patternMaker(char guess, String word) {
+        String pattern = "";
+        int index = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != guess) {
+                pattern += patternDisplayed.substring(index, index + 2);
+            } else {
+                pattern += guess + " ";
+            }
+            index += 2;
+        }
+        return pattern;
+    }
+
+    // Tracks the occurences of the letters in the pool
+    // If the occurence is zero it gets reduced by one
+    // returns the maximum occurences of the letters
+    private int decision(char guess) {
+        int maxOccurence = 0;
+        for (int i = 0; i < patternDisplayed.length(); i++) {
+            if (patternDisplayed.charAt(i) == guess) {
+                maxOccurence++;
+            }
+        }
+        lettersGuessed.add(guess);
+        if (maxOccurence == 0) {
+            guessCount--;
+        }
+        return maxOccurence;
+
 	}
 }
